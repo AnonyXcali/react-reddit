940ee0e0a4b31c56b5418c48749340ea6915a8a4
AnonyXCali <sauravezio@gmail.com>
2019-07-07 19:16:50 +0530

    added more subreddits

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,7 @@
 /*
   Update variables which are constant over the entire application
 */
+//alternative, pics, gifs, advice animals, cats, images, photoshop battles, hmmm, all, aww
 
 
 const options = [
@@ -8,9 +9,17 @@ const options = [
   { key: 'fe', text: 'frontend', value: 'frontend' },
   { key: 'ct', text: 'cats', value: 'cats' },
   { key: 'an', text: 'anime', value: 'anime' },
-  { key: 'br', text: 'barcelona', value: 'barcelona' },
   { key: 'fb', text: 'football', value: 'football' },
-  { key: 'fn', text: 'fortnite', value: 'fortnite' }
+  { key: 'alt', text: 'alternative', value: 'alternative' },
+  { key: 'pc', text: 'pics', value: 'pics' },
+  { key: 'gf', text: 'gifs', value: 'gifs' },
+  { key: 'im', text: 'images', value: 'images' },
+  { key: 'pb', text: 'photoshopbattles', value: 'photoshopbattles' },
+  { key: 'aa', text: 'adviceanimals', value: 'adviceanimals' },
+  { key: 'hm', text: 'hmmm', value: 'hmmm' },
+  { key: 'al', text: 'all', value: 'all' },
+  { key: 'aw', text: 'aww', value: 'aww' },
+
 ]
 
 
